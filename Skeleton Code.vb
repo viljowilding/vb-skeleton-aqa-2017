1339ec8042ad70e69ec12f719a21c92e44b8f193
Viljo Wilding <17092858@woodhouse.ac.uk>
2018-02-06 17:24:14 +0000

    In-file changelog
    Update in file changlog, create release 0.1.2!

--- Skeleton Code.vb
+++ Skeleton Code.vb
@@ -3,6 +3,9 @@
 ' Made AWESOME by Viljo Wilding, 2018
 
 ' Changelog:
+' -- Version 0.1.2 Wilding
+'    --- Add console colours
+'    --- Create PlantFirstSeed function
 ' -- Version 0.1.1 Wilding
 '    --- Initial code release
 '    --- Start of changelog
